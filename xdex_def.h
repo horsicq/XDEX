524dafd11587ea5714c329769b0f7a2979582ce4
horsicq <horsicq@gmail.com>
2020-11-14 01:36:10 +0100

    Fix

--- xdex_def.h
+++ xdex_def.h
@@ -103,26 +103,26 @@ struct CLASS_ITEM_DEF
     quint32 static_values_off;
 };
 
-const quint16 TYPE_HEADER_ITEM=0x0000;
-const quint16 TYPE_STRING_ID_ITEM=0x0001;
-const quint16 TYPE_TYPE_ID_ITEM=0x0002;
-const quint16 TYPE_PROTO_ID_ITEM=0x0003;
-const quint16 TYPE_FIELD_ID_ITEM=0x0004;
-const quint16 TYPE_METHOD_ID_ITEM=0x0005;
-const quint16 TYPE_CLASS_DEF_ITEM=0x0006;
-const quint16 TYPE_CALL_SITE_ID_ITEM=0x0007;
-const quint16 TYPE_METHOD_HANDLE_ITEM=0x0008;
-const quint16 TYPE_MAP_LIST=0x1000;
-const quint16 TYPE_TYPE_LIST=0x1001;
-const quint16 TYPE_ANNOTATION_SET_REF_LIST=0x1002;
-const quint16 TYPE_ANNOTATION_SET_ITEM=0x1003;
-const quint16 TYPE_CLASS_DATA_ITEM=0x2000;
-const quint16 TYPE_CODE_ITEM=0x2001;
-const quint16 TYPE_STRING_DATA_ITEM=0x2002;
-const quint16 TYPE_DEBUG_INFO_ITEM=0x2003;
-const quint16 TYPE_ANNOTATION_ITEM=0x2004;
-const quint16 TYPE_ENCODED_ARRAY_ITEM=0x2005;
-const quint16 TYPE_ANNOTATIONS_DIRECTORY_ITEM=0x2006;
-const quint16 TYPE_HIDDENAPI_CLASS_DATA_ITEM=0xF000;
+const quint16 TYPE_HEADER_ITEM                  =0x0000;
+const quint16 TYPE_STRING_ID_ITEM               =0x0001;
+const quint16 TYPE_TYPE_ID_ITEM                 =0x0002;
+const quint16 TYPE_PROTO_ID_ITEM                =0x0003;
+const quint16 TYPE_FIELD_ID_ITEM                =0x0004;
+const quint16 TYPE_METHOD_ID_ITEM               =0x0005;
+const quint16 TYPE_CLASS_DEF_ITEM               =0x0006;
+const quint16 TYPE_CALL_SITE_ID_ITEM            =0x0007;
+const quint16 TYPE_METHOD_HANDLE_ITEM           =0x0008;
+const quint16 TYPE_MAP_LIST                     =0x1000;
+const quint16 TYPE_TYPE_LIST                    =0x1001;
+const quint16 TYPE_ANNOTATION_SET_REF_LIST      =0x1002;
+const quint16 TYPE_ANNOTATION_SET_ITEM          =0x1003;
+const quint16 TYPE_CLASS_DATA_ITEM              =0x2000;
+const quint16 TYPE_CODE_ITEM                    =0x2001;
+const quint16 TYPE_STRING_DATA_ITEM             =0x2002;
+const quint16 TYPE_DEBUG_INFO_ITEM              =0x2003;
+const quint16 TYPE_ANNOTATION_ITEM              =0x2004;
+const quint16 TYPE_ENCODED_ARRAY_ITEM           =0x2005;
+const quint16 TYPE_ANNOTATIONS_DIRECTORY_ITEM   =0x2006;
+const quint16 TYPE_HIDDENAPI_CLASS_DATA_ITEM    =0xF000;
 }
 #endif // XDEX_DEF_H
