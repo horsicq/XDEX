a41868820474d706ebea38ee467f681185d06794
horsicq <horsicq@gmail.com>
2021-11-09 19:20:35 +0100

    Fix: 2021-11-09

--- xdex.cmake
+++ xdex.cmake
@@ -4,6 +4,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
 
 set(XDEX_SOURCES
     ${XBINARY_SOURCES}
-    ${CMAKE_CURRENT_LIST_DIR}/xdex.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xandroidbinary.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xdex.cpp
 )
