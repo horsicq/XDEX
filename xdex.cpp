254f081c1195007d73675692d1ca3d5f6fb9384b
horsicq <horsicq@gmail.com>
2022-02-25 17:40:45 +0100

    Fix: 2022-02-25

--- xdex.cpp
+++ xdex.cpp
@@ -1134,7 +1134,7 @@ QMap<quint64,QString> XDEX::getHeaderVersions()
     return mapResult;
 }
 
-QMap<quint64, QString> XDEX::getHeaderEndianTags()
+QMap<quint64,QString> XDEX::getHeaderEndianTags()
 {
     QMap<quint64, QString> mapResult;
 
