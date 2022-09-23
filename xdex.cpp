7d0c49e5f273bd62bcc553ec67af8afdfdcf422b
horsicq <horsicq@gmail.com>
2022-09-24 00:30:53 +0200

    Fix: 2022-09-24

--- xdex.cpp
+++ xdex.cpp
@@ -801,7 +801,7 @@ QList<XDEX_DEF::PROTO_ITEM_ID> XDEX::getList_PROTO_ITEM_ID(QList<XDEX_DEF::MAP_I
     return listResult;
 }
 
-QList<XDEX_DEF::FIELD_ITEM_ID> XDEX::getList_FIELD_ITEM_ID(QList<XDEX_DEF::MAP_ITEM> *pListMapItems, PDSTRUCT *pPdStruct)
+QList<XDEX_DEF::FIELD_ITEM_ID> XDEX::getList_FIELD_ITEM_ID(QList<XDEX_DEF::MAP_ITEM> *pListMapItems,PDSTRUCT *pPdStruct)
 {
     PDSTRUCT processDataEmpty={};
 
