cfe81f9ab51af2382109bc6bb56ffb58ee029607
horsicq <horsicq@gmail.com>
2024-02-27 21:25:18 +0100

    Update file(s): 2024-02-27

--- xdex.cmake
+++ xdex.cmake
@@ -5,5 +5,8 @@ include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
 set(XDEX_SOURCES
     ${XBINARY_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xandroidbinary.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xandroidbinary.h
     ${CMAKE_CURRENT_LIST_DIR}/xdex.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xdex.h
+    ${CMAKE_CURRENT_LIST_DIR}/xdex_def.h
 )
