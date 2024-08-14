dd71f550eb9d0581d32ebfa29d13adf3c766208f
horsicq <horsicq@gmail.com>
2024-08-15 00:24:10 +0200

    Update file(s): 2024-08-15

--- xdex.cpp
+++ xdex.cpp
@@ -786,7 +786,7 @@ QList<XDEX_DEF::STRING_ITEM_ID> XDEX::getList_STRING_ITEM_ID(QList<XDEX_DEF::MAP
 
     QByteArray baData = read_array(mapItem.nOffset, mapItem.nCount * sizeof(XDEX_DEF::STRING_ITEM_ID));
     char *pData = baData.data();
-    qint32 nSize = baData.size() / sizeof(XDEX_DEF::STRING_ITEM_ID);
+    qint32 nSize = baData.size() / (qint32)sizeof(XDEX_DEF::STRING_ITEM_ID);
 
     for (qint32 i = 0; (i < nSize) && (!(pPdStruct->bIsStop)); i++) {
         qint64 nOffset = sizeof(XDEX_DEF::STRING_ITEM_ID) * i;
