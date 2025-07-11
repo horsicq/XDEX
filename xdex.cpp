b3dafc8d6f96504c59db789c5213d785d56ad30b
horsicq <horsicq@gmail.com>
2025-07-11 08:40:49 +0200

    Update file(s): 2025-07-11

--- xdex.cpp
+++ xdex.cpp
@@ -1232,14 +1232,13 @@ QList<XBinary::FPART> XDEX::getFileParts(quint32 nFileParts, qint32 nLimit, PDST
             if (nFileParts & FILEPART_SECTION) {
                 record.nVirtualAddress = -1;
                 record.filePart = FILEPART_SECTION;
-                record.sOriginalName= mapTypes.value(mapItem.nType);
+                record.sOriginalName = mapTypes.value(mapItem.nType);
                 listResult.append(record);
             }
 
             if (record.nFileOffset + record.nFileSize > nMaxOffset) {
                 nMaxOffset = record.nFileOffset + record.nFileSize;
             }
-
         }
 
         if (nFileParts & FILEPART_OVERLAY) {
